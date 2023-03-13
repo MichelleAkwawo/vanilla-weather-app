4b7846cfc5e4cc8791209857685c0a4b0d1ce850
MichelleAkwawo <ellekennedy78@gmail.com>
2023-03-13 09:25:20 +0100

    Updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes} `;
+}
+
 function displayTemperature(response) {
   console.log(response.data.temperature.current);
 
@@ -15,6 +42,10 @@ function displayTemperature(response) {
 
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
+  );
+
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.time * 1000
   );
 }
 
